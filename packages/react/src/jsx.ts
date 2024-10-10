263255830bb295059fe6d11f85828cbd4d4e3bad
jimao <shihao.gu@perfma.com>
2024-10-11 00:11:19 +0800

    ReactElement

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -1,16 +1,17 @@
-import { REACT_ELEMENT_TYPE } from '../../shared/ReactSymbols';
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import type { Type, Key, Ref, Props, ReactElement } from 'shared/ReactTypes';
 
 // ReactElement
 
 const ReactElement = function(
-  type,
-  key,
-  ref,
-  props,
-) {
+  type: Type,
+  key: Key,
+  ref: Ref,
+  props: Props,
+): ReactElement {
   const element = {
     $$typeof: REACT_ELEMENT_TYPE,
-    key, ref, props,
+    type, key, ref, props,
     __mark: 'jimao',
   };
   return element;
